169e5fc02a316093e7471b03015ecdc3b4b8f523
jatinhemnani01 <jatinhemnani01@gmail.com>
2020-12-04 16:15:20 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_insta/flutter_insta.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  FlutterInsta flutterInsta =
+      FlutterInsta(); // create instance of FlutterInsta class
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController reelController = TextEditingController();
+  TabController tabController;
+
+  String username, following, bio, website, profileimage;
+  String enterName = "Know If You Are Eligible Or Not";
+  int followers;
+  bool pressed = false;
+  bool downloading = false;
+  Color bgcolor = Colors.indigo[300];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: homePage(),
+      appBar: AppBar(
+        backgroundColor: Color(0xffff6b6b),
+        title: const Text('Check Instagram Profile'),
+        centerTitle: true,
+      ),
+    );
+  }
+
+//get data from api
+  Future printDetails(String username) async {
+    await flutterInsta.getProfileData(username);
+    setState(() {
+      this.username = flutterInsta.username; //username
+      this.followers = flutterInsta.followers; //number of followers
+      this.following = flutterInsta.following; // number of following
+      this.website = flutterInsta.website; // bio link
+      this.bio = flutterInsta.bio; // Bio
+      this.profileimage = flutterInsta.imgurl; // Profile picture URL
+      eligble();
+    });
+  }
+
+  eligble() {
+    if (followers >= 999) {
+      setState(() {
+        enterName = "Eligible";
+        bgcolor = Colors.green;
+      });
+    } else {
+      setState(() {
+        enterName = "Not Eligible";
+        bgcolor = Colors.red;
+      });
+    }
+  }
+
+  Widget homePage() {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                hintText: "Enter Username",
+              ),
+              controller: usernameController,
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.red)),
+              color: Color(0xffff6b6b),
+              child: Text(
+                "Get Profile",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () async {
+                setState(() {
+                  pressed = true;
+                });
+                printDetails(usernameController.text);
+                //get Data
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                enterName,
+                style: TextStyle(
+                  letterSpacing: 2.0,
+                  backgroundColor: bgcolor,
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            pressed
+                ? SingleChildScrollView(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: Card(
+                        child: Container(
+                          margin: EdgeInsets.all(15),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 10),
+                              ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: Image.network(
+                                  "${profileimage}",
+                                  width: 120,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10),
+                              ),
+                              Text(
+                                "${username}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    "${followers}\nFollowers",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  Text(
+                                    "${following}\nFollowing",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10),
+                              ),
+                              Text(
+                                "${bio}",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Padding(padding: EdgeInsets.only(top: 10)),
+                              Text(
+                                "${website}",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+
+//Download reel video on button clickl
+
+}
